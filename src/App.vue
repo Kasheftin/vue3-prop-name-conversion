82e432a0878103f974a99ef6d5fdc029877ac6c8
Kasheftin <Kasheftin@gmail.com>
2022-11-27 16:55:42 +0200

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <main>
+    <!-- Working: -->
+    <PropsTest event-name="someEvent" @someEvent="handler" />
+    <PropsTest event-name="someEvent" @SomeEvent="handler" />
+    <PropsTest event-name="someEvent" @some-event="handler" />
+
+    <PropsTest event-name="SomeEvent" @someEvent="handler" />
+    <PropsTest event-name="SomeEvent" @SomeEvent="handler" />
+    <PropsTest event-name="SomeEvent" @some-event="handler" />
+
+    <PropsTest event-name="some-event" @someEvent="handler" />
+    <PropsTest event-name="some-event" @SomeEvent="handler" />
+    <PropsTest event-name="some-event" @some-event="handler" />
+
+    <PropsTest event-name="someEvent" :onSomeEvent="handler" />
+    <PropsTest event-name="SomeEvent" :onSomeEvent="handler" />
+    <PropsTest event-name="some-event" :onSomeEvent="handler" />
+
+    <br>
+    <!-- Non Working: -->
+    <PropsTest event-name="someEvent" :on-some-event="handler" />
+    <PropsTest event-name="SomeEvent" :on-some-event="handler" />
+    <PropsTest event-name="some-event" :on-some-event="handler" />
+
+  </main>
+</template>
+
+<script setup lang="ts">
+import PropsTest from './PropsTest.vue'
+
+const handler = () => {
+  console.log('handler')
+}
+</script>
+
---
+++ src/PropsTest.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <button @click="$emit(props.eventName)">
+      Click
+    </button>
+    {{ test }}
+  </div>
+</template>
+
+<script setup lang="ts">
+import { getCurrentInstance, computed } from 'vue'
+
+const props = defineProps({
+  eventName: {
+    type: String,
+    required: true
+  }
+})
+
+const vm = getCurrentInstance()
+
+const test = computed(() => {
+  return vm?.vnode?.props ? Object.keys(vm.vnode.props).join(', ') : ''
+})
+
+</script>
